89daa045e92289d12ee1f8c8d06c7d3872f74bad
HsinTsao <364893771@qq.com>
2021-07-13 10:52:57 +0800

    update

---
+++ manager.py
@@ -0,0 +1,22 @@
+from application import app, manager
+from flask_script import Server
+
+# web_server
+manager.add_command('runserver',
+                    Server(host='0.0.0.0', port=app.config['SERVER_PORT'], use_debugger=app.config['DEBUG'],
+                           use_reloader=True))
+
+
+def main():
+    manager.run()
+
+
+if __name__ == '__main__':
+    try:
+        import sys
+
+        sys.exit(main())
+    except Exception as e:
+        import traceback
+
+        traceback.print_exc()
